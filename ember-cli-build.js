e0a77c55e7c9c1832f65a6a60a3ee0f91b1ed98c
Martin Rodriguez <Martin.Rodriquez@careerbuilder.com>
2015-08-26 15:47:12 -0300

    Import kabu

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,5 +19,101 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('vendor/kabu/fonts/bitter/bitter-bold.eot', {
+    destDir: 'fonts/bitter'
+  });
+  app.import('vendor/kabu/fonts/bitter/bitter-bold.svg', {
+    destDir: 'fonts/bitter'
+  });
+  app.import('vendor/kabu/fonts/bitter/bitter-bold.ttf', {
+    destDir: 'fonts/bitter'
+  });
+  app.import('vendor/kabu/fonts/bitter/bitter-bold.woff', {
+    destDir: 'fonts/bitter'
+  });
+
+  app.import('vendor/kabu/fonts/glyphicons/glyphicons-halflings-regular.eot', {
+    destDir: 'fonts/glyphicons'
+  });
+  app.import('vendor/kabu/fonts/glyphicons/glyphicons-halflings-regular.svg', {
+    destDir: 'fonts/glyphicons'
+  });
+  app.import('vendor/kabu/fonts/glyphicons/glyphicons-halflings-regular.ttf', {
+    destdir: 'fonts/glyphicons'
+  });
+  app.import('vendor/kabu/fonts/glyphicons/glyphicons-halflings-regular.woff', {
+    destdir: 'fonts/glyphicons'
+  });
+
+  app.import('vendor/kabu/fonts/icons/icomoon.eot', {
+    destDir: 'fonts/icons'
+  });
+  app.import('vendor/kabu/fonts/icons/icomoon.svg', {
+    destDir: 'fonts/icons'
+  });
+  app.import('vendor/kabu/fonts/icons/icomoon.ttf', {
+    destDir: 'fonts/icons'
+  });
+  app.import('vendor/kabu/fonts/icons/icomoon.woff', {
+    destDir: 'fonts/icons'
+  });
+
+  app.import('vendor/kabu/fonts/lobster/lobster_1.3-webfont.eot', {
+    destDir: 'fonts/lobster'
+  });
+  app.import('vendor/kabu/fonts/lobster/lobster_1.3-webfont.svg', {
+    destDir: 'fonts/lobster'
+  });
+  app.import('vendor/kabu/fonts/lobster/lobster_1.3-webfont.ttf', {
+    destDir: 'fonts/lobster'
+  });
+  app.import('vendor/kabu/fonts/lobster/lobster_1.3-webfont.woff', {
+    destDir: 'fonts/lobster'
+  });
+
+  app.import('vendor/kabu/fonts/opensans/opensans-italic.eot', {
+    destDir: 'fonts/opensans'
+  });
+  app.import('vendor/kabu/fonts/opensans/opensans-italic.woff', {
+    destDir: 'fonts/opensans'
+  });
+  app.import('vendor/kabu/fonts/opensans/opensans-regular.eot', {
+    destDir: 'fonts/opensans'
+  });
+  app.import('vendor/kabu/fonts/opensans/opensans-regular.svg', {
+    destDir: 'fonts/opensans'
+  });
+  app.import('vendor/kabu/fonts/opensans/opensans-regular.ttf', {
+    destDir: 'fonts/opensans'
+  });
+  app.import('vendor/kabu/fonts/opensans/opensans-regular.woff', {
+    destDir: 'fonts/opensans'
+  });
+  app.import('vendor/kabu/fonts/opensans/opensans-semibold.eot', {
+    destDir: 'fonts/opensans'
+  });
+  app.import('vendor/kabu/fonts/opensans/opensans-semibold.woff', {
+    destDir: 'fonts/opensans'
+  });
+  app.import('vendor/kabu/fonts/opensans/opensans-semibolditalic.eot', {
+    destDir: 'fonts/opensans'
+  });
+  app.import('vendor/kabu/fonts/opensans/opensans-semibolditalic.woff', {
+    destDir: 'fonts/opensans'
+  });
+
+  app.import('vendor/kabu/images/credit-cards@2x.png', {
+    destDir: 'images'
+  });
+  app.import('vendor/kabu/images/spinner.gif', {
+    destDir: 'images'
+  });
+  app.import('vendor/kabu/images/spinner_dark.gif', {
+    destDir: 'images'
+  });
+
+  app.import('vendor/kabu/javascripts/kabu.min.js');
+  app.import('vendor/kabu/stylesheets/kabu.min.css');
+
   return app.toTree();
 };
