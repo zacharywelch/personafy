a14645115651ece71f21ce6af3faef17be5c109c
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-08-27 10:36:37 -0400

    remove unused routes

--- app/routes/behaviors/index.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model() {
-    let behaviors = this.modelFor('personas/edit').get('behaviors');
-    return behaviors;
-  }
-});
--- app/routes/goals/index.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model() {
-    let goals = this.modelFor('personas/edit').get('goals');
-    return goals;
-  }
-});
