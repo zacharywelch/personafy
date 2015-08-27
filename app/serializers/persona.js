058255d0ed65524322164e62a119c71e5641a739
Martin Rodriguez <Martin.Rodriquez@careerbuilder.com>
2015-08-27 18:31:05 -0300

    Fix goals and enable Save persona button when behaviors or goals are deleted

--- app/serializers/persona.js
+++ app/serializers/persona.js
@@ -3,7 +3,8 @@ import DS from 'ember-data';
 export default DS.ActiveModelSerializer.extend(DS.EmbeddedRecordsMixin, {
   isNewSerializerAPI: true,
   attrs: {
-    behaviors: { embedded: 'always' }
+    behaviors: { embedded: 'always' },
+    goals:     { embedded: 'always' }
   },
   keyForAttribute: function(attr) {
     if (attr === 'behaviors') {
