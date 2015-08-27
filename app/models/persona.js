918054d10bfd57f7910decb18772c845decd73c6
Martin Rodriguez <Martin.Rodriquez@careerbuilder.com>
2015-08-27 17:56:11 -0300

    Save button disables persona has not changed

--- app/models/persona.js
+++ app/models/persona.js
@@ -5,5 +5,27 @@ export default DS.Model.extend({
   description: DS.attr('string'),
   photoUrl: DS.attr('string'),
   behaviors: DS.hasMany('behavior', { async: false }),
-  goals: DS.hasMany('goal', { async: false })
+  goals: DS.hasMany('goal', { async: false }),
+  areBehaviorsDirty() {
+    let dirtyBehaviors = false;
+    this.get('behaviors').forEach(function (behavior) {
+      console.log(behavior.get('hasDirtyAttributes'));
+      if (behavior.get('hasDirtyAttributes')) {
+        dirtyBehaviors = true;
+      }
+    });
+
+    return dirtyBehaviors;
+  },
+  areGoalsDirty() {
+    let dirtyGoals = false;
+    this.get('goals').forEach(function (goal) {
+      console.log(goal.get('hasDirtyAttributes'));
+      if (goal.get('hasDirtyAttributes')) {
+        dirtyGoals = true;
+      }
+    });
+
+    return dirtyGoals;
+  }
 });
