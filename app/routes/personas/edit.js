6c7e4a6073dd14f3e666e52a486757c341c06eda
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-08-26 20:03:57 -0400

    edit route with save

--- app/routes/personas/edit.js
+++ app/routes/personas/edit.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    save() {
+      var model = this.modelFor('personas/edit');
+
+      model.save().then(() => {
+        this.transitionTo('personas');
+      });
+    },
+    cancel() {
+      this.transitionTo('personas');
+    }
+  }
 });
