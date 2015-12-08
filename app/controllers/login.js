aec62b59376c9ceed01c38847c5e016b972aeb0d
Martin Rodriguez <Martin.Rodriquez@careerbuilder.com>
2015-12-08 10:52:45 -0300

    Remove successfully login flash message

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -10,7 +10,6 @@ export default Ember.Controller.extend({
       const { login, password } = this.getProperties('login', 'password');
 
       this.get('authManager').authenticate('authenticator:oauth2', login, password).then(() => {
-        this.get('flashMessages').success('You have signed in successfully!');
         this.transitionTo('personas');
       }, (error) => {
         this.get('flashMessages').danger('Invalid email or password');
