498ce48d5f26310e8dc290248eb80af68af78ea6
Martin Rodriguez <Martin.Rodriquez@careerbuilder.com>
2015-12-08 10:47:51 -0300

    Add flash messages when logging in

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,15 +3,17 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   authManager: Ember.inject.service('session'),
 
+  showErrors: false,
+
   actions: {
     authenticate() {
       const { login, password } = this.getProperties('login', 'password');
 
       this.get('authManager').authenticate('authenticator:oauth2', login, password).then(() => {
-        console.log('transitioning');
+        this.get('flashMessages').success('You have signed in successfully!');
         this.transitionTo('personas');
-      }, (err) => {
-        alert('Error obtaining token: ' + err.responseText);
+      }, (error) => {
+        this.get('flashMessages').danger('Invalid email or password');
       });
     }
   }
