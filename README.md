e71330b1df325dc7b50841ffcbf070cafb04c4ec
Martin Rodriguez <merodriguezblanco@gmail.com>
2016-03-03 18:38:03 -0300

    Rename project name to Personafy

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Personas
+# Personafy
 
 This README outlines the details of collaborating on this Ember application.
 A short introduction of this app could easily go here.
--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'personas',
+    modulePrefix: 'personafy',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
